05ec3c9bc79071042399e31f0001615c4d1e0887
Titus Wormer <tituswormer@gmail.com>
2018-07-20 18:06:10 -0400

    .

---
+++ index.js
@@ -0,0 +1,187 @@
+'use strict'
+
+/* eslint-disable no-constant-condition */
+
+// See https://tools.ietf.org/html/rfc4647#section-3.1
+// for more information on the algorithms.
+
+var dash = '-'
+var asterisk = '*'
+
+exports.basicFilter = factory(basic, true)
+exports.extendedFilter = factory(extended, true)
+exports.lookup = factory(lookup)
+
+// Basic Filtering (Section 3.3.1) matches a language priority list consisting
+// of basic language ranges (Section 2.1) to sets of language tags.
+function basic(tag, range) {
+  tag = lower(tag)
+  range = lower(range)
+  return range === asterisk || tag === range || tag.indexOf(range + dash) !== -1
+}
+
+// Extended Filtering (Section 3.3.2) matches a language priority list
+// consisting of extended language ranges (Section 2.2) to sets of language
+// tags.
+function extended(tag, range) {
+  // 3.3.2.1
+  var tags = lower(tag).split(dash)
+  var ranges = lower(range).split(dash)
+  var length = ranges.length
+  var rangeIndex = -1
+  var tagIndex = -1
+
+  tag = tags[++tagIndex]
+  range = ranges[++rangeIndex]
+
+  // 3.3.2.2
+  if (range !== asterisk && range !== tag) {
+    return false
+  }
+
+  tag = tags[++tagIndex]
+  range = ranges[++rangeIndex]
+
+  // 3.3.2.3
+  while (rangeIndex < length) {
+    // 3.3.2.3.A
+    if (range === asterisk) {
+      range = ranges[++rangeIndex]
+      continue
+    }
+
+    // 3.3.2.3.B
+    if (!tag) {
+      return false
+    }
+
+    // 3.3.2.3.C
+    if (tag === range) {
+      tag = tags[++tagIndex]
+      range = ranges[++rangeIndex]
+      continue
+    }
+
+    // 3.3.2.3.D
+    if (tag.length === 1) {
+      return false
+    }
+
+    // 3.3.2.3.E
+    tag = tags[++tagIndex]
+  }
+
+  // 3.3.2.4
+  return true
+}
+
+// Lookup (Section 3.4) matches a language priority list consisting of basic
+// language ranges to sets of language tags to find the one exact language tag
+// that best matches the range.
+function lookup(tag, range) {
+  var pos
+
+  tag = lower(tag)
+  range = lower(range)
+
+  while (true) {
+    if (range === asterisk || tag === range) {
+      return true
+    }
+
+    pos = range.lastIndexOf(dash)
+
+    if (pos === -1) {
+      return false
+    }
+
+    if (pos > 3 && range.charAt(pos - 2) === dash) {
+      pos -= 2
+    }
+
+    range = range.substring(0, pos)
+  }
+}
+
+// Factory to perform a filter or a lookup.
+// This factory creates a function that accepts a list of tags and a list of
+// ranges, and contains logic to exit early for lookups.
+// `check` just has to deal with one tag and one range.
+// This match function iterates over ranges, and for each range,
+// iterates over tags.  That way, earlier ranges matching any tag have
+// precedence over later ranges.
+function factory(check, filter) {
+  return match
+
+  function match(tags, ranges) {
+    var values = normalize(tags, ranges)
+    var result = []
+    var next
+    var tagIndex
+    var tagLength
+    var tag
+    var rangeIndex
+    var rangeLength
+    var range
+    var matches
+
+    tags = values.tags
+    ranges = values.ranges
+    rangeLength = ranges.length
+    rangeIndex = -1
+
+    while (++rangeIndex < rangeLength) {
+      range = ranges[rangeIndex]
+
+      // Ignore wildcards in lookup mode.
+      if (!filter && range === asterisk) {
+        continue
+      }
+
+      tagLength = tags.length
+      tagIndex = -1
+      next = []
+
+      while (++tagIndex < tagLength) {
+        tag = tags[tagIndex]
+        matches = check(tag, range)
+        ;(matches ? result : next).push(tag)
+
+        // Exit if this is a lookup and we have a match.
+        if (!filter && matches) {
+          return tag
+        }
+      }
+
+      tags = next
+    }
+
+    // If this is a filter, return the list.  If it’s a lookup, we didn’t find
+    // a match, so return `undefined`.
+    return filter ? result : undefined
+  }
+}
+
+// Normalize options.
+function normalize(tags, ranges) {
+  ranges = ranges === undefined || ranges === null ? asterisk : ranges
+
+  return {tags: cast(tags, 'tag'), ranges: cast(ranges, 'range')}
+}
+
+// Validate tags or ranges, and cast them to arrays.
+function cast(values, name) {
+  var value = values && typeof values === 'string' ? [values] : values
+
+  if (!value || typeof value !== 'object' || !('length' in value)) {
+    throw new Error(
+      'Invalid ' + name + ' `' + value + '`, expected non-empty string'
+    )
+  }
+
+  return value
+}
+
+function lower(value) {
+  return value.toLowerCase()
+}
---
+++ test.js
@@ -0,0 +1,217 @@
+'use strict'
+
+var test = require('tape')
+var chalk = require('chalk')
+var match = require('.')
+
+var basic = match.basicFilter
+var extended = match.extendedFilter
+var lookup = match.lookup
+
+test('basic(tags[, ranges="*"])', function(t) {
+  ;[
+    ['de-de', null, ['de-de']],
+    ['de-de', '*', ['de-de']],
+    ['de-DE', '*', ['de-DE']],
+    ['de-DE-1996', '*', ['de-DE-1996']],
+    ['de-Deva', '*', ['de-Deva']],
+    ['de-Latn-DE', '*', ['de-Latn-DE']],
+    ['de', 'de', ['de']],
+    ['DE', 'de', ['DE']],
+    ['de', 'de', ['de']],
+    ['de-DE', 'de', ['de-DE']],
+    ['DE-DE', 'de', ['DE-DE']],
+    ['de-de', 'de-de', ['de-de']],
+    ['de-DE', 'de-de', ['de-DE']],
+    ['de-de', 'de-DE', ['de-de']],
+    ['de-DE-1996', 'de-de', ['de-DE-1996']],
+    ['de-Deva', 'de-de', []],
+    ['de-Latn-DE', 'de-de', []],
+    ['en', 'de-de', []],
+    ['en', 'de', []],
+    ['de-CH-1996', 'de-CH', ['de-CH-1996']],
+    ['nb', 'no', []],
+    ['zh-Hans', 'zh-CN', []],
+    [['de-de', 'de', 'en-GB', 'en'], 'de', ['de-de', 'de']],
+    [['de-de', 'de', 'en-GB', 'en'], 'en', ['en-GB', 'en']],
+    [
+      ['de-de', 'de', 'en-GB', 'en'],
+      ['de', 'en'],
+      ['de-de', 'de', 'en-GB', 'en']
+    ],
+    [
+      ['de-de', 'de', 'en-GB', 'en'],
+      ['en', 'de'],
+      ['en-GB', 'en', 'de-de', 'de']
+    ],
+    [['de-de', 'de', 'en-GB', 'en'], ['en-gb', 'de-de'], ['en-GB', 'de-de']],
+    [['de-de', 'de', 'en-GB', 'en'], ['de-de', 'de'], ['de-de', 'de']]
+  ].forEach(check(t, basic))
+
+  t.throws(
+    function() {
+      basic()
+    },
+    /^Error: Invalid tag `undefined`, expected non-empty string$/,
+    'should throw without tag'
+  )
+
+  t.throws(
+    function() {
+      basic('')
+    },
+    /^Error: Invalid tag ``, expected non-empty string$/,
+    'should throw with empty string tag'
+  )
+
+  t.throws(
+    function() {
+      basic(1)
+    },
+    /^Error: Invalid tag `1`, expected non-empty string$/,
+    'should throw with invalid tag'
+  )
+
+  t.end()
+})
+
+test('extended(tags[, ranges="*""])', function(t) {
+  ;[
+    ['de-de', null, ['de-de']],
+    ['de-de', '*', ['de-de']],
+    ['de-DE', '*', ['de-DE']],
+    ['de-Latn-DE', '*', ['de-Latn-DE']],
+    ['de-Latf-DE', '*', ['de-Latf-DE']],
+    ['de-DE-x-goethe', '*', ['de-DE-x-goethe']],
+    ['de-Latn-DE-1996', '*', ['de-Latn-DE-1996']],
+    ['de-Deva-DE', '*', ['de-Deva-DE']],
+    ['de', '*', ['de']],
+    ['de-x-DE', '*', ['de-x-DE']],
+    ['de-Deva', '*', ['de-Deva']],
+    ['de-DE', 'de-DE', ['de-DE']],
+    ['de-de', 'de-DE', ['de-de']],
+    ['de-Latn-DE', 'de-DE', ['de-Latn-DE']],
+    ['de-Latf-DE', 'de-DE', ['de-Latf-DE']],
+    ['de-DE-x-goethe', 'de-DE', ['de-DE-x-goethe']],
+    ['de-Latn-DE-1996', 'de-DE', ['de-Latn-DE-1996']],
+    ['de-Deva-DE', 'de-DE', ['de-Deva-DE']],
+    ['de', 'de-DE', []],
+    ['de-x-DE', 'de-DE', []],
+    ['de-Deva', 'de-DE', []],
+    ['en', 'de-de', []],
+    ['en', 'de', []],
+    ['de-CH-1996', 'de-CH', ['de-CH-1996']],
+    ['nb', 'no', []],
+    ['zh-Hans', 'zh-CN', []],
+    ['de-DE', 'de-*-DE', ['de-DE']],
+    ['de-de', 'de-*-DE', ['de-de']],
+    ['de-Latn-DE', 'de-*-DE', ['de-Latn-DE']],
+    ['de-Latf-DE', 'de-*-DE', ['de-Latf-DE']],
+    ['de-DE-x-goethe', 'de-*-DE', ['de-DE-x-goethe']],
+    ['de-Latn-DE-1996', 'de-*-DE', ['de-Latn-DE-1996']],
+    ['de-Deva-DE', 'de-*-DE', ['de-Deva-DE']],
+    ['de', 'de-*-DE', []],
+    ['de-x-DE', 'de-*-DE', []],
+    ['de-Deva', 'de-*-DE', []],
+
+    [['de-de', 'de', 'en-GB', 'en'], 'de', ['de-de', 'de']],
+    [['de-de', 'de', 'en-GB', 'en'], 'en', ['en-GB', 'en']],
+    [
+      ['de-de', 'de', 'en-GB', 'en'],
+      ['de', 'en'],
+      ['de-de', 'de', 'en-GB', 'en']
+    ],
+    [
+      ['de-de', 'de', 'en-GB', 'en'],
+      ['en', 'de'],
+      ['en-GB', 'en', 'de-de', 'de']
+    ],
+    [['de-de', 'de', 'en-GB', 'en'], ['en-gb', 'de-de'], ['en-GB', 'de-de']],
+    [['de-de', 'de', 'en-GB', 'en'], ['de-de', 'de'], ['de-de', 'de']],
+    [
+      ['de', 'de-de', 'de-DE-x-goethe', 'de-Deva', 'de-Deva-DE', 'de-x-DE'],
+      'de-*-DE',
+      ['de-de', 'de-DE-x-goethe', 'de-Deva-DE']
+    ]
+  ].forEach(check(t, extended))
+
+  t.throws(
+    function() {
+      extended()
+    },
+    /^Error: Invalid tag `undefined`, expected non-empty string$/,
+    'should throw without tag'
+  )
+
+  t.throws(
+    function() {
+      extended('')
+    },
+    /^Error: Invalid tag ``, expected non-empty string$/,
+    'should throw with empty string tag'
+  )
+
+  t.throws(
+    function() {
+      extended(1)
+    },
+    /^Error: Invalid tag `1`, expected non-empty string$/,
+    'should throw with invalid tag'
+  )
+
+  t.end()
+})
+
+test('lookup(tags[, ranges="*"])', function(t) {
+  ;[
+    // Wildcards have no effect in `lookup`
+    ['de-de', null, undefined],
+    ['de-de', '*', undefined],
+    ['de-DE', '*', undefined],
+    ['en-GB', 'de-ch', undefined],
+    [['en-GB', 'de-CH-1996'], 'de-ch', undefined],
+    [['en-GB', 'de-CH'], 'de-ch', 'de-CH'],
+    [['en-GB', 'de'], 'de-ch', 'de'],
+    [['de-CH', 'fr-CH', 'it-CH'], '*-CH', 'de-CH'],
+    [['en-GB', 'en', 'ja-JP', 'ja'], ['fr-FR', 'zh-Hant'], undefined],
+    [
+      ['en-GB', 'en', 'zh-Hans', 'zh-Hant', 'zh'],
+      ['fr-FR', 'zh-Hant'],
+      'zh-Hant'
+    ],
+    [
+      ['en-GB', 'en', 'zh-Hans', 'zh-Hant', 'zh', 'fr'],
+      ['fr-FR', 'zh-Hant'],
+      'fr'
+    ],
+    [
+      ['en-GB', 'en', 'zh-Hans', 'zh-Hant', 'zh', 'fr'],
+      ['zh-Hant', 'fr-FR'],
+      'zh-Hant'
+    ],
+    [
+      ['en-GB', 'en', 'zh-Hans', 'zh-Hant', 'zh'],
+      'zh-Hant-CN-x-private1-private2',
+      'zh-Hant'
+    ]
+  ].forEach(check(t, lookup))
+
+  t.end()
+})
+
+function check(t, fn) {
+  return checker
+
+  function checker(opts) {
+    t.deepEqual(
+      fn(opts[0], opts[1]),
+      opts[2],
+      'f(' +
+        opts[0] +
+        '; ' +
+        chalk.bold.green(opts[1]) +
+        ') -> ' +
+        chalk.bold(String(opts[2]) || '[]')
+    )
+  }
+}
